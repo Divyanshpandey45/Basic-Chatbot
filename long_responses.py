d6070f9e461bf51c7e4bc60d2ec0aa928dca1a82
Divyanshpandey45 <divyanshpandey2072005@gmail.com>
2024-06-18 19:37:07 +0530

    Add files via upload

---
+++ long_responses.py
@@ -0,0 +1,7 @@
+import random
+R_EATING = "i don't like eating anaything because i'm a boy obviously!"
+def unknown():
+    response = ['Could you plrase re-phrase that?',
+                "...",
+                "Sounds about right"
+                "What dose that mean?"][random.randrange(4)]
